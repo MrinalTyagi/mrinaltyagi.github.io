b8caafb8224ce0a466fbdd2ea66fdc4c9e07efc7
Mrinal Tyagi <mrinaltyagi24@gmail.com>
2024-09-07 10:51:48 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // JavaScript functionality can be added here if needed in the future.
+    console.log('Webpage loaded successfully.');
+});
